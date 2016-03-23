eac67ac91eb04ed99ac9692a722da098394644c0
Ming <bearmingo@gmail.com>
2016-03-23 20:29:42 +0800

    Add font setting

--- init.el
+++ init.el
@@ -34,6 +34,15 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; English fonts
+(set-face-attribute
+ 'default nil :font "Monaco 11")
+ ;; Chinese fonts
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset
+                    (font-spec :family "Microsoft Yahei" :size 14)))
+                    
 ;; Load custom modules
 (require 'setup-plugins)
 (require 'setup-faces)
